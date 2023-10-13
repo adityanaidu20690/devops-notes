b3f3b895fee85980321fdd0760f501bc2f6ceedb
adityanaidu20690 <adityanaidu20690@gmail.com>
2023-10-13 16:57:33 +0530

    updated

--- terraform-notes/creating-instance-aws-deconstruct/instances.tf
+++ terraform-notes/creating-instance-aws-deconstruct/instances.tf
@@ -1,18 +1,19 @@
 resource "aws_instance" "addy" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  key_name               = aws_key_pair.developer.key_name
+  key_name               = "${aws_key_pair.developer.key_name}"
   vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
   tags = {
     Name = "first-instance"
   }
-  user_data = <<-EOF
-  #!/bin/bash
-  yum update -y
-  yum install httpd -y
-  systemctl enable httpd
-  systemctl start httpd
-  echo "hello world" > /var/www/html/index.html
+  # user_data = <<-EOF
+  # #!/bin/bash
+  # yum update -y
+  # yum install httpd -y
+  # systemctl enable httpd
+  # systemctl start httpd
+  # echo "hello world" > /var/www/html/index.html
 
-  EOF
+  # EOF
+  user_data = file("${path.module}/shell.sh")
 }
