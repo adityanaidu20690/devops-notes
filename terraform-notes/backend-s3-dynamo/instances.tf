33f4924a64ab3e17807fef77c383cd5b1d58ff50
adityanaidu20690 <adityanaidu20690@gmail.com>
2023-10-19 18:34:54 +0530

    Remote Backend State Locking using S3 and DynamoDB

---
+++ terraform-notes/backend-s3-dynamo/instances.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "addy" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = "${aws_key_pair.developer.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
+  tags = {
+    Name = "first-instance"
+  }
+
+}
+
+# terraform {
+#   backend "s3" {
+#     dynamodb_table = "addy-xlr8"
+#     bucket = "addy-bucket-786"
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"
+    
+#   }
+# } 
